4733ea25c0a7ffd0cdeb99084ac936cf53d8fe0c
Jacob Koziej <jacobkoziej@gmail.com>
2021-12-09 16:56:39 -0500

    Add copyright notice

--- p2/p2.cc
+++ p2/p2.cc
@@ -111,6 +111,27 @@ int main() {
 // You may add global variables, functions, and/or
 // class defintions here if you wish.
 
+/*
+ * p2.cc -- programming assignment #2
+ *          sort nodes of a linked list by the fields of last name,
+ *          first name, and social security number of a hypothetical
+ *          person
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <climits>
 
 
